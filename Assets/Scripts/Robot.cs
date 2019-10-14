eb98b56275fdef60763312959ae80d800a295348
Tanner Olsen <tanner.olsen@student.sl.on.ca>
2019-10-14 14:44:14 -0400

    added in robot noises and spawns - end of chapter 10

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Robot : MonoBehaviour
 {
-
+    [SerializeField]
+    private AudioClip deathSound;
+    [SerializeField]
+    private AudioClip fireSound;
+    [SerializeField]
+    private AudioClip weakHitSound;
     [SerializeField]
     GameObject missileprefab;
     [SerializeField]
@@ -57,6 +62,7 @@ public class Robot : MonoBehaviour
         missile.transform.position = missileFireSpot.transform.position;
         missile.transform.rotation = missileFireSpot.transform.rotation;
         robot.Play("Fire");
+        GetComponent<AudioSource>().PlayOneShot(fireSound);
     }
 
     public void TakeDamage(int amount)
@@ -71,6 +77,11 @@ public class Robot : MonoBehaviour
             isDead = true;
             robot.Play("Die");
             StartCoroutine("DestroyRobot");
+            GetComponent<AudioSource>().PlayOneShot(deathSound);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(weakHitSound);
         }
     }
     IEnumerator DestroyRobot()
